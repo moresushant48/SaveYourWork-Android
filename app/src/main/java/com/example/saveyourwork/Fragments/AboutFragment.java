4c8ee118f9c220faa3189d914ea3f5ad6e683928
Sushant More <moresushant48@gmail.com>
2019-10-27 03:15:54 +0530

    Drawer : About - Complete
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- app/src/main/java/com/example/saveyourwork/Fragments/AboutFragment.java
+++ app/src/main/java/com/example/saveyourwork/Fragments/AboutFragment.java
@@ -1,21 +1,53 @@
 package com.example.saveyourwork.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.saveyourwork.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AboutFragment extends Fragment {
+
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about,container,false);
+        view = inflater.inflate(R.layout.fragment_about,container,false);
+
+        ImageView imageView = view.findViewById(R.id.imgGit);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/moresushant48/"));
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[] {"moresushant48@gmail.com"});
+                email.setType("message/rfc822");
+                startActivity(email);
+
+            }
+        });
+
+        return view;
     }
 }
